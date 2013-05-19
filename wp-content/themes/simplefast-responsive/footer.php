d6d9a77536b2c7e2435c16c07df2221d0b30541b
arya <sedekahin@gmail.com>
2013-05-20 03:04:19 +0700

    remove babilink

--- wp-content/themes/simplefast-responsive/footer.php
+++ wp-content/themes/simplefast-responsive/footer.php
@@ -8,16 +8,5 @@ Copyright &#169; 2012  <a href="<?php echo home_url() ; ?>"><?php bloginfo('name
 <?php static_footer_pages(); ?>
 </footer>
 </div>
-<script type="text/javascript">
-  var vglnk = { api_url: '//api.viglink.com/api',
-                key: 'c5d8f82b94ccf34b3c04d4ff2e0d11cb' };
-
-  (function(d, t) {
-    var s = d.createElement(t); s.type = 'text/javascript'; s.async = true;
-    s.src = ('https:' == document.location.protocol ? vglnk.api_url :
-             '//cdn.viglink.com/api') + '/vglnk.js';
-    var r = d.getElementsByTagName(t)[0]; r.parentNode.insertBefore(s, r);
-  }(document, 'script'));
-</script>
 </body>
 </html>
